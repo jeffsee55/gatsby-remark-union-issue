7d378a1004535d135ce9a17c23ae47f916a5931c
Jeff See <jeffsee@Jeffs-MacBook-Air.local>
2019-11-07 14:14:54 -0800

    Working example of manually creating unions

---
+++ contents/blog/first-blog.md
@@ -0,0 +1,16 @@
+---
+date: 2019-02-13
+title: First Blog
+rootPage: /blog
+sidebar: Blog
+showTitle: true
+hideAnchor: true
+blocks: 
+- template: block__hero
+  heroField1: '## Hello world'
+  heroField2: '## Hello world'
+- template: block__columns
+  columnField1: '## Hello again'
+  columnField2: '## Hello again'
+---
+[Next Blog](/blog/second-blog)
---
+++ gatsby-node.js
@@ -0,0 +1,57 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+exports.createPages = require('./gatsby/createPages')
+exports.onCreateNode = require('./gatsby/onCreateNode')
+
+exports.createSchemaCustomization = ({ actions: { createTypes }, schema }) => {
+  const typeDefs = [
+    "type MarkdownRemark implements Node { frontmatter: Frontmatter }",
+    schema.buildObjectType({
+      name: "BlockHero",
+      fields: {
+        template: 'String!',
+        heroField1: 'String'
+      },
+      interfaces: ["Node"],
+    }),
+    schema.buildObjectType({
+      name: "BlockColumns",
+      fields: {
+        template: 'String!',
+        columnField1: 'String!',
+      },
+      interfaces: ["Node"],
+    }),
+    schema.buildUnionType({
+      name: "HeroColumnUnion",
+      types: ['BlockHero', 'BlockColumns' ],
+      resolveType(value) {
+        if (value.template === 'block__hero') {
+          return 'BlockHero';
+        }
+        if (value.template === 'block__columns') {
+          return 'BlockColumns';
+        }
+
+        throw 'No template defined'
+      }
+    }),
+    schema.buildObjectType({
+      name: "Frontmatter",
+      fields: {
+        blocks: {
+          type: ['HeroColumnUnion'],
+        }
+      },
+    }),
+  ]
+  createTypes(typeDefs)
+};
+
+
